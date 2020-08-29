c62c7e71c0bcca5f6fe3e5f05d08218f998bbc6d
subalakshmi <67703467+shuba03@users.noreply.github.com>
2020-08-29 17:22:10 +0530

    increment&decrement JS file

---
+++ Basic.js
@@ -0,0 +1,12 @@
+function myDecrement()
+{ 
+  var x = parseInt(document.getElementById("value1").value);
+  var z=x-1;
+  value1.value=z;
+}
+function myIncrement()
+{
+  var x = parseInt(document.getElementById("value1").value);
+  var z=x+1;
+  value1.value=z;
+}
